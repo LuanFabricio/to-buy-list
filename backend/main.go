7cb3a00669aae813872386ba8eb66e290b1b09af
LuanFabricio <luanlimaleite@hotmail.com>
2024-04-20 23:54:47 -0300

    feat: Adding user controller on gin router

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"tbl-backend/controller/buy_item"
-	"tbl-backend/database"
 	"tbl-backend/controller/to_buy_list"
+	"tbl-backend/controller/user"
+	"tbl-backend/database"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +23,7 @@ func main() {
 	router.DELETE("/buy_items/:id", buy_item.DeleteBuyItem)
 	router.POST("/buy_items", buy_item.PostBuyItem)
 	router.GET("/to_buy_list", to_buy_list.GetToBuyList)
+	router.POST("/user", user.PostUser)
 
 	router.Run("localhost:3000")
 }
