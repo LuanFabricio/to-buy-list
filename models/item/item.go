f4151dc4f0e6e248b6fdfd542ae588e13f0067ee
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-01 17:26:48 -0300

    feat: Adding the FindItem and Update function for buy item

--- models/item/item.go
+++ models/item/item.go
@@ -36,6 +36,24 @@ func FindItems(db* sql.DB)  ([]BuyItem, error) {
 	return items, nil
 }
 
+func FindItem(id string, db* sql.DB) (*BuyItem, error) {
+	row, err := db.Query("SELECT id, name, current_quantity, min_quantity, send_email FROM items WHERE id=$1", id)
+	defer row.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var item BuyItem
+	row.Next()
+	err = row.Scan(&item.ID, &item.Name, &item.CurrentQuantity, &item.MinQuantity, &item.SendEmail)
+	if err != nil {
+		return nil, err
+	}
+
+	return &item, nil
+}
+
 func (bi* BuyItem) Insert(db* sql.DB) (*BuyItem, error) {
 	var (
 		id uint32
@@ -63,4 +81,27 @@ func (bi* BuyItem) Insert(db* sql.DB) (*BuyItem, error) {
 		MinQuantity: uint32(min_quantity),
 		SendEmail: send_email,
 	}, nil
+}
+
+func (bi* BuyItem) Update(db* sql.DB) (*BuyItem, error) {
+	res, err := db.Query(`
+		UPDATE items
+		SET name = $1, current_quantity = $2, min_quantity = $3, send_email = $4
+		WHERE id = $5
+		RETURNING *`,
+		bi.Name, bi.CurrentQuantity,
+		bi.MinQuantity, bi.SendEmail,
+		bi.ID,
+	);
+
+	if err != nil{
+		return nil, err;
+	}
+
+	if res.Next() {
+		res.Scan(&bi.ID, &bi.Name, &bi.CurrentQuantity, &bi.MinQuantity, &bi.SendEmail)
+		return bi, nil
+	}
+
+	return nil, nil
 }
