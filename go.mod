a2445dad4a49c3a08dd7b8146cbbc804de6a8fdf
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-11 15:02:49 -0300

    feat: Adding a cron job scheduler and testing SendToBuyListEmail

--- go.mod
+++ go.mod
@@ -14,11 +14,14 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-co-op/gocron/v2 v2.5.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -26,10 +29,12 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.5.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
 	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"tbl-backend/controllers/buy_item"
-	"tbl-backend/controllers/pages"
-	"tbl-backend/controllers/to_buy_list"
-	"tbl-backend/controllers/user"
 	"log"
-	"github.com/joho/godotenv"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-co-op/gocron/v2"
+	"github.com/joho/godotenv"
+
+	"tbl-backend/controllers/buy_item"
+	"tbl-backend/controllers/pages"
+	"tbl-backend/controllers/user"
+	"tbl-backend/database"
+	"tbl-backend/services/to_buy_list"
 )
 
 func main() {
@@ -17,6 +21,39 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+
+	err = to_buy_list.SendToBuyListEmail(database.GetDbConnection())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	job, err := scheduler.NewJob(
+		gocron.DailyJob(
+			1,
+			gocron.NewAtTimes(
+				gocron.NewAtTime(12, 00, 00),
+				gocron.NewAtTime(13, 12, 00),
+			),
+		),
+		gocron.NewTask(
+			to_buy_list.SendToBuyListEmail,
+			database.GetDbConnection(),
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Printf("Job ID: %v", job.ID())
+	scheduler.Start()
+	defer scheduler.Shutdown()
 
 	router := gin.Default()
 
@@ -36,7 +73,7 @@ func main() {
 	router.PUT("/buy_items/:id", buy_item.PutBuyItem)
 	router.DELETE("/buy_items/:id", buy_item.DeleteBuyItem)
 	router.POST("/buy_items", buy_item.PostBuyItem)
-	router.GET("/to_buy_list", to_buy_list.GetToBuyList)
+	// router.GET("/to_buy_list", to_buy_list.GetToBuyList)
 	router.POST("/user", user.PostUser)
 
 	router.Run("localhost:3000")
