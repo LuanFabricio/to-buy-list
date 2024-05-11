efdca8c6e9fa093f922486646bb34b394eecdfd9
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-11 15:01:47 -0300

    feat: Creating SendToBuyListEmail function
    Create a function to send email to all of the users in a specific
    buy list

--- services/to_buy_list/to_buy_list.go
+++ services/to_buy_list/to_buy_list.go
@@ -2,8 +2,51 @@ package to_buy_list
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"tbl-backend/models/item"
+	"tbl-backend/services/email"
 )
+
+func SendToBuyListEmail(db *sql.DB) error {
+	to_buy_list, err := FetchToBuyList(db)
+
+	if err != nil {
+		return err
+	}
+
+	emailContent := `
+	<h1>To Buy items</h1>
+	<ol>
+	`
+	for i, to_buy_item := range to_buy_list {
+		delta := int32(to_buy_item.CurrentQuantity) - int32(to_buy_item.MinQuantity)
+		emailContent += fmt.Sprintf(
+			"<li>%s: %d/%d(%d)</li>",
+			to_buy_item.Name,
+			to_buy_item.CurrentQuantity,
+			to_buy_item.MinQuantity,
+			delta,
+		)
+
+		if i+1 < len(to_buy_list) { emailContent += "\r\n" }
+	}
+	emailContent += "</ol>"
+
+	log.Println(emailContent)
+
+	to, err := email.FetchUsersEmail(db)
+
+	if err != nil {
+		return err
+	}
+
+	return email.SendEmail(
+		to,
+		"To Buy List",
+		emailContent+"IDK",
+	)
+}
 
 func FetchToBuyList(db *sql.DB) ([]item.BuyItem, error) {
 	buy_list_row, err := db.Query("SELECT * FROM items")
