ef587f75c8f2aef9306436c4552571d801c7535a
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-18 17:44:29 -0300

    feat: Adding ExtractTokenId function and moving to user ID

--- services/token/token.go
+++ services/token/token.go
@@ -2,6 +2,7 @@ package token
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -10,12 +11,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GenerateToken(username string) (string, error){
+func GenerateToken(userId string) (string, error){
 	tokenLifeSpan := 4
 
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
-	claims["user_name"] = username
+	claims["id"] = userId
 	claims["exp"] = time.Now().Add(time.Hour * time.Duration(tokenLifeSpan)).Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -45,4 +46,28 @@ func ExtractToken(c *gin.Context) string {
 	}
 
 	return ""
+}
+
+func ExtractTokenId(tokenString string) (string, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("TBL_API_SECRET")), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	log.Printf("[INFO]: claims %v\n", claims)
+	log.Printf("[INFO]: ok %v\n", ok)
+	if ok && token.Valid {
+		userId := fmt.Sprintf("%s", claims["id"])
+		log.Printf("[INFO]: userID: %s", userId)
+		return userId, nil
+	}
+
+	return "", nil
 }
