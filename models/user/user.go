5446c79c11aa95cccb3913f5a840a709bc8a564e
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-18 18:14:23 -0300

    feat: Adding FetchUserByID and an user FetchBuyLists method

--- models/user/user.go
+++ models/user/user.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"database/sql"
+	"log"
+	"tbl-backend/models/buy_list"
 )
 
 type User struct {
@@ -31,6 +33,17 @@ func (ud* UserDTO) Insert(db* sql.DB) (*User, error) {
 	return &user, nil
 }
 
+func FetchUser(db* sql.DB, id string) (User, error){
+	var u User
+	err := db.QueryRow(
+		`SELECT id, username, password FROM users
+		WHERE id = $1`,
+		id,
+	).Scan(&u.ID, &u.Username, &u.Password)
+
+	return u, err
+}
+
 func (u* User) FindByEmail(db* sql.DB, email string) (error) {
 	err := db.QueryRow(
 		`SELECT id, username, password FROM users
@@ -43,4 +56,31 @@ func (u* User) FindByEmail(db* sql.DB, email string) (error) {
 	}
 
 	return nil
+}
+
+func (u* User) FetchByLists(db* sql.DB) []buylist.BuyList {
+	buyListArr := make([]buylist.BuyList, 0)
+
+	rows, err := db.Query(
+		`SELECT bl.id, bl.name, bl.owner_user_id FROM buy_list_access bla
+		JOIN buy_list bl ON bl.id = bla.buy_list_id
+		WHERE bla.user_id = $1`,
+		u.ID,
+	)
+	if err != nil {
+		log.Printf("[ERROR]: %v", err)
+		return buyListArr
+	}
+
+	var buyList buylist.BuyList
+	for rows.Next() {
+		err = rows.Scan(&buyList.ID, &buyList.Name, &buyList.OwnerUserID)
+		if err != nil {
+			log.Printf("[WARNING]: %v", err)
+		} else {
+			buyListArr = append(buyListArr, buyList)
+		}
+	}
+
+	return buyListArr
 }
