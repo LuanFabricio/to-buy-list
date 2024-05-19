1c679b62a07af6263c74291d90918838c515c226
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-19 10:30:57 -0300

    feat: Add a FetchItems method

--- models/buy_list/buy_list.go
+++ models/buy_list/buy_list.go
@@ -2,6 +2,8 @@ package buylist
 
 import (
 	"database/sql"
+	"log"
+	"tbl-backend/models/item"
 )
 
 type BuyList struct {
@@ -23,4 +25,31 @@ func (bl* BuyList) FetchByID(db* sql.DB, id int) error {
 	}
 
 	return nil
+}
+
+func (bl* BuyList) FetchItems(db* sql.DB) ([]item.BuyItem, error){
+	buyItemArr := make([]item.BuyItem, 0)
+
+	rows, err := db.Query(
+		`SELECT id, name, current_quantity, min_quantity, send_email FROM items
+		WHERE buy_list_id = $1
+		ORDER BY (current_quantity-min_quantity)`,
+		bl.ID,
+	)
+	if err != nil {
+		return buyItemArr, err
+	}
+
+	var buyItem item.BuyItem
+	for rows.Next() {
+		err = rows.Scan(&buyItem.ID, &buyItem.Name, &buyItem.CurrentQuantity, &buyItem.MinQuantity, &buyItem.SendEmail)
+
+		if err != nil {
+			log.Printf("[ERROR]: %v\n", err)
+		} else {
+			buyItemArr = append(buyItemArr, buyItem)
+		}
+	}
+
+	return buyItemArr, nil
 }
