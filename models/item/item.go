3e6fe43600dd12f08e9925b524d2aac97402fede
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-23 19:12:55 -0300

    feat: Adding buy list id field into item model

--- models/item/item.go
+++ models/item/item.go
@@ -10,6 +10,7 @@ import (
 
 type BuyItem struct {
 	ID string `json:"id"`
+	BuyListId string `json:"list_id"`
 	Name string `json:"name"`
 	CurrentQuantity uint32 `json:"current_quantity"`
 	MinQuantity uint32 `json:"min_quantity"`
@@ -17,7 +18,7 @@ type BuyItem struct {
 }
 
 func FindItems(db* sql.DB, order_by_id bool)  ([]BuyItem, error) {
-	select_query := "SELECT id, name, current_quantity, min_quantity, send_email FROM items"
+	select_query := "SELECT id, name, current_quantity, min_quantity, send_email, buy_list_id  FROM items"
 
 	if order_by_id {
 		select_query += " ORDER BY id"
@@ -33,7 +34,14 @@ func FindItems(db* sql.DB, order_by_id bool)  ([]BuyItem, error) {
 	var item BuyItem
 	items := []BuyItem{}
 	for row.Next() {
-		err = row.Scan(&item.ID, &item.Name, &item.CurrentQuantity, &item.MinQuantity, &item.SendEmail)
+		err = row.Scan(
+			&item.ID,
+			&item.Name,
+			&item.CurrentQuantity,
+			&item.MinQuantity,
+			&item.SendEmail,
+			&item.BuyListId,
+		)
 		if err != nil {
 			return nil, err
 		}
@@ -45,7 +53,12 @@ func FindItems(db* sql.DB, order_by_id bool)  ([]BuyItem, error) {
 }
 
 func FindItem(id string, db* sql.DB) (*BuyItem, error) {
-	row, err := db.Query("SELECT id, name, current_quantity, min_quantity, send_email FROM items WHERE id=$1", id)
+	row, err := db.Query(`
+		SELECT
+			id, name,
+			current_quantity, min_quantity,
+			send_email, buy_list_id
+		FROM items WHERE id=$1`, id)
 	defer row.Close()
 
 	if err != nil {
@@ -54,7 +67,14 @@ func FindItem(id string, db* sql.DB) (*BuyItem, error) {
 
 	var item BuyItem
 	row.Next()
-	err = row.Scan(&item.ID, &item.Name, &item.CurrentQuantity, &item.MinQuantity, &item.SendEmail)
+	err = row.Scan(
+		&item.ID,
+		&item.Name,
+		&item.CurrentQuantity,
+		&item.MinQuantity,
+		&item.SendEmail,
+		&item.BuyListId,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -69,14 +89,15 @@ func (bi* BuyItem) Insert(db* sql.DB) (*BuyItem, error) {
 		current_quantity uint64
 		min_quantity uint64
 		send_email bool
+		buy_list_id uint32
 	)
 
 	err := db.QueryRow(
 		`INSERT INTO items (name, current_quantity, min_quantity, send_email, buy_list_id)
-		VALUES($1, $2, $3, $4, 1)
-		RETURNING id, name, current_quantity, min_quantity, send_email`,
-		bi.Name, bi.CurrentQuantity, bi.MinQuantity, bi.SendEmail,
-	).Scan(&id, &name, &current_quantity, &min_quantity, &send_email)
+		VALUES($1, $2, $3, $4, $5)
+		RETURNING id, name, current_quantity, min_quantity, send_email, buy_list_id`,
+		bi.Name, bi.CurrentQuantity, bi.MinQuantity, bi.SendEmail, bi.BuyListId,
+	).Scan(&id, &name, &current_quantity, &min_quantity, &send_email, &buy_list_id)
 
 	if err != nil {
 		return nil, err
@@ -88,18 +109,24 @@ func (bi* BuyItem) Insert(db* sql.DB) (*BuyItem, error) {
 		CurrentQuantity: uint32(current_quantity),
 		MinQuantity: uint32(min_quantity),
 		SendEmail: send_email,
+		BuyListId: fmt.Sprint(buy_list_id),
 	}, nil
 }
 
 func (bi* BuyItem) Update(db* sql.DB) (*BuyItem, error) {
 	res, err := db.Query(`
 		UPDATE items
-		SET name = $1, current_quantity = $2, min_quantity = $3, send_email = $4
-		WHERE id = $5
+		SET
+			name = $1,
+			current_quantity = $2,
+			min_quantity = $3,
+			send_email = $4,
+			buy_list_id = $5
+		WHERE id = $6
 		RETURNING *`,
 		bi.Name, bi.CurrentQuantity,
 		bi.MinQuantity, bi.SendEmail,
-		bi.ID,
+		bi.BuyListId, bi.ID,
 	);
 
 	if err != nil{
@@ -107,7 +134,17 @@ func (bi* BuyItem) Update(db* sql.DB) (*BuyItem, error) {
 	}
 
 	if res.Next() {
-		res.Scan(&bi.ID, &bi.Name, &bi.CurrentQuantity, &bi.MinQuantity, &bi.SendEmail)
+		err := res.Scan(
+			&bi.ID,
+			&bi.Name,
+			&bi.CurrentQuantity,
+			&bi.MinQuantity,
+			&bi.SendEmail,
+			&bi.BuyListId,
+		)
+		if err != nil {
+			return nil, err
+		}
 		return bi, nil
 	}
 
@@ -131,6 +168,10 @@ func (bi* BuyItem) LoadFromForm(c *gin.Context) {
 	if minQuantity := c.PostForm("min_quantity"); minQuantity != "" {
 		minQuantity, _ := strconv.ParseUint(minQuantity, 10, 32)
 		bi.MinQuantity = uint32(minQuantity)
+	}
+
+	if buyListId := c.PostForm("buy_list_id"); buyListId != "" {
+		bi.BuyListId = buyListId
 	}
 
 	postSendEmail := c.PostForm("send_email")
